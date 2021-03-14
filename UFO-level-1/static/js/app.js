d2b9d9205ff83a3a8b21d00b22d130a31a8a6152
sir-omoreno <ozmoreno@gmail.com>
2021-03-13 19:22:29 -0500

    creating table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,4 +3,22 @@ var tableData = data;
 
 // YOUR CODE HERE!
 
-var button = d3.select("#button")
+// Inputting table data into website
+
+var table_body = d3.select("tbody")
+
+tableData.forEach((item) => {
+    var row = table_body.append("tr");
+    Object.entries(item).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// var button = d3.selectAll("button").on("click", function() {
+//     // What will be logged out? What is `this` in this case?
+//     console.log(this);
+//     // Answer: It will console log the `button` element.
+//   });
+
+
