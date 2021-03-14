189f16d4bda6c13f2870fed9e7ead8e763786c94
sir-omoreno <ozmoreno@gmail.com>
2021-03-13 20:38:05 -0500

    testing items to rows

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -23,4 +23,36 @@ tableData.forEach((item) => {
 
 
 var button = d3.select("#filter-btn");
-var form = d3.select(".form-control");
+button.on("click", enter_date_function);
+
+function enter_date_function() {
+    var inputDate = d3.select("#datetime").property("value")
+
+    var filterData = tableData.filter(siting => siting.datetime === inputDate)
+
+    var tbody = d3.select("tbody")
+    tbody.html("")
+
+    if (filterData.length == 0) {
+        alert(`check date format.`);
+    } else {
+        // Add the data if the date is correct
+        filterData.forEach((ufo_siting) => {
+            // add row
+            var row = d3.select("tbody").append("tr")
+
+            // go thorugh the filtered data
+            Object.entries(ufo_siting).forEach(([key, value]) => {
+                // add cell in each row
+                var cell = row.append("td")
+                cell.text(value)
+            })
+        })}
+    };
+
+
+
+
+
+
+
